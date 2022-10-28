36f7240a3385afb55ae097c5c46a8b6a36b0dccf
Philippe Poulard <philippe.poulard@inria.fr>
2022-10-28 19:45:36 +0200

    Add Test Class

---
+++ test/class.test.ts
@@ -0,0 +1,31 @@
+import { Class, Namespace } from "../src";
+
+describe('Class', () => {
+    describe('rename()', () => {
+
+        test('w/o ns', () => {
+            class MyClass {}
+            const YourClass = Class.rename(MyClass, 'YourClass');
+            expect(MyClass.name).toBe('MyClass');
+            expect(YourClass.name).toBe('YourClass');
+        });
+
+        test('with @Namespace', () => {
+            class RootClass {}
+            @Namespace(RootClass)
+            class ParentClass {}
+            @Namespace(ParentClass)
+            class ChildClass {}
+            expect(Namespace.getQualifiedName(RootClass)).toBe('RootClass');
+            expect(Namespace.getQualifiedName(ParentClass)).toBe('RootClass.ParentClass');
+            expect(Namespace.getQualifiedName(ChildClass)).toBe('RootClass.ParentClass.ChildClass');
+            const ClassParent = Class.rename(ParentClass, 'ClassParent');
+            expect(Namespace.getQualifiedName(RootClass)).toBe('RootClass');
+            expect(Namespace.getQualifiedName(ClassParent)).toBe('RootClass.ClassParent');
+            expect(Namespace.getQualifiedName(ChildClass)).toBe('RootClass.ClassParent.ChildClass');
+            expect(Namespace.getQualifiedName(ParentClass)).toBe('ParentClass');
+        });
+
+    });
+
+});
